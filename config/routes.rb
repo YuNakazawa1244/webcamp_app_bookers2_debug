c61a3981d71ff4b1a122681c172e1c7934bfb96c
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-04-01 13:40:33 +0000

    [Start]bookers2_debug

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  #以下デバッグ、記載順の入れ替え
+
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+  # resources :users,only: [:show,:index,:edit,:update]
+  # resources :books
+  # devise_for :users
+  # root 'homes#top'
+  # get 'home/about' => 'homes#about'
+  #以上デバッグ、要質問
+
+# 以下デバッグ、end追加
+end
+# デバッグ以上
